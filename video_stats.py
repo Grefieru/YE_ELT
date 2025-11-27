06c2abdcb739b8d54199b788e9cd5d9526f1d80d
CostinR DevOps <osti.rzv@gmail.com>
2025-11-27 10:00:13 +0200

    The script is done, saving data in a json format

--- video_stats.py
+++ video_stats.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from datetime import date
 
 import os
 from dotenv import load_dotenv
@@ -25,7 +26,7 @@ def get_playlist_id():
         channel_items = data["items"][0]
         channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"]['uploads']
 
-        print(channel_playlistId)
+        #print(channel_playlistId)
 
         return channel_playlistId
 
@@ -87,7 +88,7 @@ def extract_video_data(video_ids):
             video_ids_str = ",".join(batch)
 
             url = f'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}'
-            
+
             response = requests.get(url)
 
             response.raise_for_status()
@@ -118,8 +119,16 @@ def extract_video_data(video_ids):
         raise e
 
 
+def save_to_json(extracted_data):
+    file_path = f"./data/YT_data{date.today()}.json"
+
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
+
+
 if __name__ == "__main__":
     playlistId = get_playlist_id()
     video_ids = get_video_ids(playlistId)
-    print(extract_video_data(video_ids))
+    video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
 
