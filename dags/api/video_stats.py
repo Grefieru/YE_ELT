23578acc1cef31dfe9578712fd6b0f02a666b3c0
CostinR DevOps <osti.rzv@gmail.com>
2025-11-28 12:46:36 +0200

    DAGs are working, everything looks fine atm

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -2,15 +2,19 @@ import requests
 import json
 from datetime import date
 
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path="./.env")
 
-load_dotenv(dotenv_path="./.env")
+from airflow.decorators import task
+from airflow.models import Variable
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "MrBeast"
+
+API_KEY = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 maxResults = 50
 
+@task
 def get_playlist_id():
     try:
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
@@ -35,7 +39,7 @@ def get_playlist_id():
     
 
 
-
+@task
 def get_video_ids(playlistId):
 
     video_ids = []
@@ -73,7 +77,7 @@ def get_video_ids(playlistId):
     except requests.exceptions.RequestException as e:
         raise e
 
-
+@task
 def extract_video_data(video_ids):
     extracted_data = []
 
@@ -118,9 +122,9 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
-
+@task
 def save_to_json(extracted_data):
-    file_path = f"./data/YT_data{date.today()}.json"
+    file_path = f"./data/YT_data_{date.today()}.json"
 
     with open(file_path, "w", encoding="utf-8") as json_outfile:
         json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
